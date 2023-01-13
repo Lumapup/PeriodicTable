8cff238c2c23e3088c13f93ea9ba618a2b2c0f1b
Lumapup <etmathews2018@gmail.com>
2023-01-13 03:58:14 +0000

    feat: addeelement checks

--- element.sh
+++ element.sh
@@ -6,6 +6,21 @@ if [[ -z $1 ]]
 then
   echo Please provide an element as an argument.
 else
-  echo "Argument \"$1\" found"
+  #Argument given, check if it's a number.
+  ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$1;")
+  #If blank, proceed to next check:
+  if [[ -z $ATOMIC_NUMBER ]]
+  then
+    #Check if it's a symbol.
+    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1';")
+    #If blank, proceed to final check:
+    if [[ -z $ATOMIC_NUMBER ]]
+    then
+      #Check if it's a name.
+      ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE name='$1';")
+      #If still blank, if statement below will deal with it
+    fi
+  fi
+  #Once number is gotten (no matter how), proceed on
 fi
 
