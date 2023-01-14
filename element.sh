f6baae15c8ac53832e8463091d3f25fbdf7ff098
Lumapup <etmathews2018@gmail.com>
2023-01-14 01:25:20 +0000

    feat: added variable setting and info echoing

--- element.sh
+++ element.sh
@@ -33,7 +33,16 @@ then
   #If atomic number found, output info
   if [[ ! -z $ATOMIC_NUMBER ]]
   then
-    echo "Atomic number $ATOMIC_NUMBER found"
+    #Get all variables
+    NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number=$ATOMIC_NUMBER;")
+    SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number=$ATOMIC_NUMBER;")
+    ATOMIC_MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$ATOMIC_NUMBER;")
+    TYPE=$($PSQL "SELECT type FROM types FULL JOIN properties USING(type_id) WHERE atomic_number=$ATOMIC_NUMBER;")
+    MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER;")
+    BOILING_POINT=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER;")
+
+    #Show information
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
   else
     echo "I could not find that element in the database."
   fi
